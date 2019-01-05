b592a1746a9b1531a44e00313e538659b46ee126
Bill Zeligson <>
2019-01-05 16:45:48 -0500

    update warehousing copy

--- src/modules/warehousing/warehousing.jsx
+++ src/modules/warehousing/warehousing.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const warehousing = () => (
 	<div className="candor-content candor-page warehousing">
 		<div className="candor-title">Warehouse & Distribution Operations</div>
-		<div className="candor-paragraph">How does having the ability to concentrate on your core business sound? In other words, do what you do best. It's a major advantage to outsource your warehousing to Candor Transportation and Warehousing.</div>
+		<div className="candor-paragraph">How does having the ability to concentrate on your core business sound? In other words, do what you do best. It's a major advantage to outsource your warehousing to Candor Logistics and Warehousing.</div>
     <img src="/static/images/warehousing-high.jpg" />
 		<div className="candor-paragraph">Public warehousing allows you the flexibility of converting many of your fixed costs to variable costs. As your need for more space increases or, decreases, public warehousing can be flexible enough to accommodate these situations. Outsourcing your warehousing to Candor Transportation and Warehousing can turn many of your warehousing challenges into advantages for your company.</div>
 		<div className="candor-paragraph">Candor Warehouse has a full range of services with rack and bulk storage as well as yard storage for those items that can be stored outdoors. Strategically located in Cleveland near the 271/480 interchange, we can provide you with complete regional distribution to your customers within a 250 mile radius on our own trucks.</div>
